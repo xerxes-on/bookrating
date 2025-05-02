ca494831f69085a6d3f2296354ccf4fd18018cdc
xerxes-on <122219058+xerxes-on@users.noreply.github.com>
2025-05-02 10:49:08 +0500

    s

---
+++ public/build/assets/Logout-BA2XvHPq.js
@@ -0,0 +1,1 @@
+import{k as t}from"./app-CT_gfoAL.js";const _={__name:"Logout",setup(o){return t().logout(),(e,r)=>" Logging out :( "}};export{_ as default};
---
+++ public/build/assets/profile-CrnoEbjE.js
@@ -0,0 +1,1 @@
+import{c as o}from"./client-t_DSb9yI.js";const s=()=>o.post("/profile").then(t=>t).catch(t=>t.response),r=t=>o.post("/quotes/"+t).then(e=>e).catch(e=>e.response),p={getProfileDetails:s,getQuote:r};export{p};
